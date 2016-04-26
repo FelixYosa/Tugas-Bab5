61a8087e2168539a19c9ee42990e243e34c3c968
FelixYosa <felix_yosa@ymail.com>
2016-04-26 19:50:59 +0700

    edit

--- src/tugas/bab5/Penghitungan.java
+++ src/tugas/bab5/Penghitungan.java
@@ -5,21 +5,26 @@ public class Penghitungan {
         
     public static void penjumlahan(int a, int b){
         double jum = a+b;
+        System.out.println("Hasil penjumlahan : "+jum);
     }
     
     public static void pengurangan(int a, int b){
         double kurang = a-b;
+        System.out.println("Hasil pengurangan : "+kurang);
     }
     
     public void perkalian(int a, int b){
         double kali = a*b;
+        System.out.println("Hasil perkalian : "+kali);
     }
     
     public void pembagian(int a, int b){
         double bagi = a/b;
+        System.out.println("Hasil pembagian : "+bagi);
     }
     
     public void sederhana(int a, int b){
         double desimal = a/b;
+        System.out.println("Hasil penyederhanaan : "+desimal);
     }
 }
