a65bd96f30399d913b7c2f42c726b1daeb0edc8d
FelixYosa <felix_yosa@ymail.com>
2016-04-26 19:47:38 +0700

    edit

--- src/tugas/bab5/Penghitungan.java
+++ src/tugas/bab5/Penghitungan.java
@@ -2,24 +2,24 @@
 package tugas1;
 
 public class Penghitungan {
-    
-    public static void penjumlahan(){
-        jum = a+b;
+        
+    public static void penjumlahan(int a, int b){
+        double jum = a+b;
     }
     
-    public static void pengurangan(){
-        kurang = a-b;
+    public static void pengurangan(int a, int b){
+        double kurang = a-b;
     }
     
-    public void perkalian(){
-        kali = a*b;
+    public void perkalian(int a, int b){
+        double kali = a*b;
     }
     
-    public void pembagian(){
-        bagi = a/b;
+    public void pembagian(int a, int b){
+        double bagi = a/b;
     }
     
-    public void sederhana(){
-        desimal = c/d;
+    public void sederhana(int a, int b){
+        double desimal = a/b;
     }
 }
