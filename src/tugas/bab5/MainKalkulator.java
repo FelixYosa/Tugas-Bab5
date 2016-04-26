b3612e449dbb13be4960672d8b460af86ab5446e
FelixYosa <felix_yosa@ymail.com>
2016-04-26 19:54:58 +0700

    buat menu

--- src/tugas/bab5/MainKalkulator.java
+++ src/tugas/bab5/MainKalkulator.java
@@ -6,7 +6,20 @@ import java.util.Scanner;
 public class MainKalkulator{
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int pilih;
+        do{
+            System.out.println("===KALKULATOR FELIX===");
+            System.out.println("1. Penjumlahan");
+            System.out.println("2. Pengurangan");
+            System.out.println("3. Perkalian");
+            System.out.println("4. Pembagian");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu : ");
+            pilih = in.nextInt();
+            
+            
+        }while(pilih != 0);
     }
     
 }
