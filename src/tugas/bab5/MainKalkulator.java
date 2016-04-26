2aa1509d457e189223319aee4b360901e493dfc4
FelixYosa <felix_yosa@ymail.com>
2016-04-26 20:31:02 +0700

    edit main

--- src/tugas/bab5/MainKalkulator.java
+++ src/tugas/bab5/MainKalkulator.java
@@ -1,3 +1,6 @@
+//Nama : Felix Yosa H. S.
+//NIM : 155150400111129
+//Kelas : SI-H
 package tugas1;
 
 import java.util.Scanner;
@@ -11,14 +14,14 @@ public class MainKalkulator {
         Kalkulator kalkulator = new Kalkulator();
 
         do {
-            System.out.println("===KALKULATOR FELIX===");
-            System.out.println("1. Penjumlahan");
-            System.out.println("2. Pengurangan");
-            System.out.println("3. Perkalian");
-            System.out.println("4. Pembagian");
+            System.out.println("");
+            System.out.println("========KALKULATOR FELIX========");
+            System.out.println("1. Penjumlahan \t\t 3. Perkalian");
+            System.out.println("2. Pengurangan \t\t 4. Pembagian");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu : ");
             pilih = in.nextInt();
+            System.out.println("");
 
             switch (pilih) {
                 case 1:
@@ -71,6 +74,14 @@ public class MainKalkulator {
                     System.out.print("Masukkan penyebut: ");
                     int bil2 = in.nextInt();
                     kalkulator.sederhana(bil1, bil2);
+                    break;
+
+                case 0:
+                    System.out.println("Terima Kasih");
+                    break;
+
+                default:
+                    System.out.println("Maaf pilihan anda tidak ada");
                     break;
             }
         } while (pilih != 0);
