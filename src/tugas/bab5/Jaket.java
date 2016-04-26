b67a52c501e96d38d637887e717e28a8ce6a1f33
FelixYosa <felix_yosa@ymail.com>
2016-04-26 20:53:21 +0700

    edit

--- src/tugas/bab5/Jaket.java
+++ src/tugas/bab5/Jaket.java
@@ -1,8 +1,36 @@
-
 package tugas2;
 
 public class Jaket {
+
     public final int bahanA = 100000;
     public final int bahanB = 125000;
     public final int bahanC = 175000;
+    public final int diskonA = 95000;
+    public final int diskonB = 120000;
+    public final int diskonC = 160000;
+    public double total;
+
+    public void setTotal(int jum, int bahan) {
+        if (jum <= 100) {
+            if (bahan == 1) {
+                total = jum * bahanA;
+            } else if (bahan == 2) {
+                total = jum * bahanB;
+            } else {
+                total = jum * bahanC;
+            }
+        } else {
+            if (bahan == 1) {
+                total = jum * diskonA;
+            } else if (bahan == 2) {
+                total = jum * diskonB;
+            } else {
+                total = jum * diskonC;
+            }
+        }
+    }
+
+    public double getTotal() {
+        return total;
+    }
 }
