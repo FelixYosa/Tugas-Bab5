9de2e904fe99343f2c7c5d01068eabe6044cbee6
FelixYosa <felix_yosa@ymail.com>
2016-04-26 20:32:34 +0700

    edit class

--- src/tugas/bab5/Kalkulator.java
+++ src/tugas/bab5/Kalkulator.java
@@ -1,10 +1,13 @@
+//Nama : Felix Yosa H. S.
+//NIM : 155150400111129
+//Kelas : SI-H
 package tugas1;
 
-public class Kalkulator{
-    
+public class Kalkulator {
+
     public static void penjumlahan(int a[]) {
         int jum = 0;
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 1; i < a.length; i++) {
             jum = jum + a[i];
         }
         System.out.println("Hasil penjumlahan : " + jum);
@@ -12,24 +15,32 @@ public class Kalkulator{
 
     public static void pengurangan(int a[]) {
         int kurang = 0;
-        for (int i = 0; i < a.length; i++) {
-            kurang = kurang + a[i];
+        for (int i = 1; i < a.length; i++) {
+            if (i == 1) {
+                kurang = kurang + a[i];
+            } else {
+                kurang = kurang - a[i];
+            }
         }
         System.out.println("Hasil pengurangan : " + kurang);
     }
 
     public void perkalian(int a[]) {
-        double kali = 0;
-        for (int i = 0; i < a.length; i++) {
-            kali = kali + a[i];
+        double kali = 1;
+        for (int i = 1; i < a.length; i++) {
+            kali = kali * a[i];
         }
         System.out.println("Hasil perkalian : " + kali);
     }
 
     public void pembagian(int a[]) {
         double bagi = 0;
-        for (int i = 0; i < a.length; i++) {
-            bagi = bagi + a[i];
+        for (int i = 1; i < a.length; i++) {
+            if (i == 1) {
+                bagi = bagi + a[i];
+            } else {
+                bagi = bagi / a[i];
+            }
         }
         System.out.println("Hasil pembagian : " + bagi);
     }
