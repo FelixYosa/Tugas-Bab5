3847d278710104c8626a4564b9557dc944a781b6
FelixYosa <felix_yosa@ymail.com>
2016-04-26 21:03:49 +0700

    buat main jaket

---
+++ src/tugas/bab5/MainJaket.java
@@ -0,0 +1,20 @@
+
+package tugas2;
+
+import java.util.Scanner;
+
+public class MainJaket {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("=========CV.LABKOMDAS=========");
+        System.out.println("===INDUSTRI PEMBUATAN JAKET===");
+        System.out.println("1. Bahan A  Rp. 100.000,00");
+        System.out.println("2. Bahan B  Rp. 125.000,00");
+        System.out.println("3. Bahan C  Rp. 175.000,00");
+        System.out.println("Diskon jika pembelian lebih dari 100");
+
+    }
+    
+}
